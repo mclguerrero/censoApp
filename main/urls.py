c956d3c4eea0143ae04be0a5a647761ae0860818
mclguerrero <maycolguerrero2021@itp.edu.co>
2024-10-28 15:34:36 -0500

    Configura Swagger: Para la documentación de la API, incluyendo schema_view y rutas para Swagger y ReDoc.

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,40 @@
 # urls
 
+from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CensoApp API",
+        default_version='v1',
+        description="Documentación de la API",
+        contact=openapi.Contact(email="soporte@censoapp.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+
+    path('api/v1/', include('main.api.v1.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
 
     #select2
 
     path("select2/", include("django_select2.urls")),
+
+    #rol user
+
+    path('myData/', views.ver_infopersonal, name='ver_infopersonal'),
+    path('myFamily/', views.ver_mifamilia, name='ver_mifamilia'),
 
     #usuarios
 
