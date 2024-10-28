979394972421e2129063723e887d02bf4b1dee1d
mclguerrero <maycolguerrero2021@itp.edu.co>
2024-10-28 15:34:36 -0500

    Crea decorador para admin: Implementación de un decorador para verificar si el usuario pertenece al grupo 'Admin'.

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,9 +1,16 @@
 # main/context_processors.py
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import user_passes_test
+
+def is_user_admin(user):
+    return user.groups.filter(name='Admin').exists()
+
+# Decorador que utiliza la función de verificación
+admin_required = user_passes_test(is_user_admin)
 
 def is_admin(request):
     if request.user.is_authenticated:
-        is_admin = request.user.groups.filter(name='Admin').exists()
+        is_admin = is_user_admin(request.user)
     else:
         is_admin = False
     return {'is_admin': is_admin}
