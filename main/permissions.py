61b0240185fef9f26404f4ce4c87707020f5bb93
mclguerrero <maycolguerrero2021@itp.edu.co>
2024-10-28 15:34:37 -0500

    Agrega permisos para REST Framework: Configuración de permisos para la API en REST Framework.

---
+++ main/permissions.py
@@ -0,0 +1,6 @@
+# main/permissions.py
+from rest_framework import permissions
+
+class IsAdminGroup(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.groups.filter(name='Admin').exists()
